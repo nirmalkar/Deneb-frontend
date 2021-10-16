f0d1c9b61c318e687f0998204b60e8084f2ea20d
nirmalkar <hemantnirmalkar17@gmail.com>
2021-10-16 23:03:28 +0530

    removed analytics tab

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -7,7 +7,7 @@ import { GoalsProvider } from 'contexts/GoalsContext'
 const Dashboard = React.lazy(() => import('containers/Dashboard'))
 const Goals = React.lazy(() => import('containers/Goals'))
 const Goal = React.lazy(() => import('containers/Goals/components/Goal'))
-const Analytics = React.lazy(() => import('containers/Analytics'))
+// const Analytics = React.lazy(() => import('containers/Analytics'))
 const LiveStuff = React.lazy(() => import('containers/LiveStuff'))
 const DesignSystem = React.lazy(() => import('components/DesignSystem'))
 
@@ -18,7 +18,7 @@ const Routes = () => {
             <Suspense fallback={<div>Loading...</div>}>
                 <Route exact path="/dashboard" component={Dashboard} />
                 <Route exact path="/design-system" component={DesignSystem} />
-                <Route exact path="/analytics" component={Analytics} />
+                {/* <Route exact path="/analytics" component={Analytics} /> */}
                 <Route exact path="/live-stuff" component={LiveStuff} />
                 <GoalsProvider>
                     <Route exact path="/goals" component={Goals} />
